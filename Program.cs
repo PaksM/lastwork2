8cce63cb6618cf88fb46bc7d5dcd95aab02c601e
Maxim <mpazyuk@mail.ru>
2022-11-14 20:53:07 +0400

    second

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+string[] MyArray = {"Яр", "Сом", "Москва", "Уфа", "Оренбург"};
+ 
+var result = new string[MyArray.Length];
+var realSize = 0;
+foreach (var value in MyArray)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
